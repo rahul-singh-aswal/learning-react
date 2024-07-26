03770cc307d4f3c33d83e40317fdcd52ae02a4e1
Rahul Singh Aswal <aswalrahul576@gmail.com>
2024-07-26 14:08:00 +0530

    basics of react

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./chai"
 
 function App() { 
+  const username = "chai aur code"
   return (
     <>
-    <h2>Chai aur React</h2>
+    <h2>Chai aur React | {username}</h2>
     <Chai />
     <p>Test Paragraph</p>
     
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// import {jsx as _jsx} from "react/jsx-dev-runtime.js"
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+const anotherElement  = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", 
+    target: '_blank'
+  },
+  'Click me to visit Google',
+  anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // anotherElement
+  reactElement
+  // <App />
 )
